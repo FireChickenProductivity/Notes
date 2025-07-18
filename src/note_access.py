8fc363a2140b2be5a24376a648641c8dabbd31f0
Fire Chicken <firechickenproductivity@gmail.com>
2025-07-18 11:38:32 -0600

    Save current note

--- src/note_access.py
+++ src/note_access.py
@@ -11,15 +11,21 @@ def get_note(name: str) -> Note:
 	raise ValueError(f"Tried to access nonexistent note {name}")
 	
 
+current_note: Note | None = None
+
 module = Module()
 @module.action_class
 class Actions:
 	def display_note_brief_by_name(name: str):
 		"""Displays the first part of the note with the name"""
+		global current_note
 		note = get_note(name)
+		current_note = note
 		actions.user.notes_display_brief(note)
 
 	def display_note_by_name(name: str):
 		"""Displays the note with the name"""
+		global current_note
 		note = get_note(name)
+		current_note = note
 		actions.user.notes_display(note)
